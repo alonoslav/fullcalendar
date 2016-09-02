f5cb1ed97632916952dd94171c859f5192bec58c
alonoslav <vadym.vorobel@gmail.com>
2016-09-02 09:40:58 +0300

    Initial commit

---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'jss:reactive-fullcalendar',
+  summary: 'Meteor Reactive - Full-sized drag & drop event calendar (jQuery plugin - v2.1.1)',
+  version: '1.1.0',
+  git: 'https://github.com/alonoslav/fullcalendar.git',
+});
+
+// eslint-disable-next-line
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@1.0');
+
+  api.use('templating', 'client');
+
+  api.addFiles([
+    'fullcalendar.min.js',
+    'fullcalendar.min.css',
+    'lang-all.js',
+    'gcal.js',
+    'scheduler.min.css',
+    'scheduler.min.js',
+    'reactive-fullcalendar-template.html',
+    'reactive-fullcalendar-template.js',
+  ], 'client');
+});
---
+++ reactive-fullcalendar-template.js
@@ -0,0 +1,48 @@
+var reactiveFullcalendar = function (options) {
+  if (!jQuery.isFunction(options.events)) {
+    throw new SyntaxError('Need events options declaring a function to manage reactive data');
+  }
+
+  var calendarEl = document.createElement('div');
+
+  calendarEl.id = options.id || 'fullCalendar';
+  calendarEl.className = `calendar ${options.addedClasses}`;
+
+  jQuery(`#${calendarEl.id}_wrapper`).append(calendarEl);
+
+  var calendar = jQuery(calendarEl).fullCalendar(options);
+
+  var autorunFunctions = function () {
+    if (options.autoruns) {
+      options.autoruns.forEach(function (funcDef) {
+        funcDef();
+      });
+    }
+  };
+
+  var update = function () {
+    calendar.fullCalendar('refetchEvents');
+  };
+
+  return {
+    autorunFunctions,
+    update,
+  };
+};
+
+
+Template.ReactiveFullcalendar.rendered = function () {
+  var data = this.data;
+  var calendar = reactiveFullcalendar(data.options);
+
+  this.autorun(function () {
+    calendar.update();
+    calendar.autorunFunctions();
+  });
+};
+
+Template.ReactiveFullcalendar.helpers({
+  id: function () {
+    return (this.options && this.options.id || "fullCalendar") + "_wrapper";
+  }
+});
